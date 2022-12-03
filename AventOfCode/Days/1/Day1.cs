cd4d026c8923159d9e457ba3050ddb4acefa78a9
Ryan Cole <ryan@rycole.com>
2022-12-02 20:31:42 -0600

    first commit

---
+++ AventOfCode/Days/1/Day1.cs
@@ -0,0 +1,37 @@
+﻿namespace AventOfCode.Days
+{
+    internal static class Day1
+    {
+        public static void Run()
+        {
+            var input = File.ReadAllLines("days\\1\\input.txt");
+
+            var elves = new Dictionary<int, int>();
+
+            var currentElf = 1;
+
+            foreach (var line in input)
+            {
+                // move to next elf
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    currentElf += 1;
+                    continue;
+                }
+
+                // init array for current elf
+                else if (elves.ContainsKey(currentElf) == false)
+                {
+                    elves[currentElf] = 0;
+                }
+
+                var calories = int.Parse(line);
+
+                // add calories to current elv
+                elves[currentElf] += calories;
+            }
+
+            var sorted = elves.OrderByDescending(m => m.Value);
+        }
+    }
+}
---
+++ AventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using AventOfCode.Days;
+
+namespace AventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
